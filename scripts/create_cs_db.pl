53e9727c9b2af1d938bf0c9ae049650fab490683
Michael Remmert <remmert@genzentrum.lmu.de>
2011-08-04 12:58:46 +0200

    Update scripts to generate HHblits databases (use BASENAME)

--- scripts/create_cs_db.pl
+++ scripts/create_cs_db.pl
@@ -29,7 +29,8 @@ Usage: perl create_cs_db.pl -i <dir> [options]
 
 Options:
   -i <dir>    Input directory with HMMER-, HMM- or A3M-files
-  -o <file>   Output file for the CS-database (default: <indir>.cs219)
+  -o <file>   Output basename for the CS-database (default: <indir>)
+              (will generate output in BASENAME.cs219)
 
   -ext <ext>  File extension, which identifies file-typ (default: $ext)
               - A3M-type  : a3m
@@ -59,7 +60,7 @@ my $options="";
 for (my $i=0; $i<@ARGV; $i++) {$options.=" $ARGV[$i] ";}
 
 if ($options=~s/ -i\s+(\S+) //) {$indir=$1;}
-if ($options=~s/ -o\s+(\S+) //) {$outfile=$1;}
+if ($options=~s/ -o\s+(\S+) //) {$outfile="$1.cs219";}
 
 if ($options=~s/ -ext\s+(\S+) //) {$ext=$1;}
 
@@ -80,7 +81,7 @@ if (!$outfile) {
     } else {
 	$outfile = "$indir.cs219";
     }
-    print("Create HHblits database with CS-database in file $outfile!\n");
+    print("Create HHblits CS-database in file $outfile!\n");
 }
 
 # Create tmp directory (plus path, if necessary)
--- scripts/create_db.pl
+++ scripts/create_db.pl
@@ -42,9 +42,9 @@ Options:
   -hhmdir <dir>  Input directory (directories) with HHM- or HMMER-files 
                  (WARNING! Using HMMER databases could result in a decreased sensitivity!)
 
-  -oa3m  <FILE>  Output filename for the A3M database 
+  -oa3m  <FILE>  Output basename for the A3M database (output will be BASENAME_a3m_db)
                  (if not given, no A3M database will be build)
-  -ohhm  <FILE>  Output filename for the HHM database 
+  -ohhm  <FILE>  Output basename for the HHM database (output will be BASENAME_hhm_db)
                  (if not given, no HHM database will be build)
 
   -a3mext        Extension of A3M-files (default: $a3mext)
@@ -56,11 +56,11 @@ Options:
 
 Examples:
 
-   perl create_db.pl -a3mdir /databases/scop_a3ms -oa3m /databases/scop_a3m_db -ohhm /databases/scop_hhm_db
+   perl create_db.pl -a3mdir /databases/scop_a3ms -oa3m /databases/scop -ohhm /databases/scop
 
-   perl create_db.pl -a3mdir /databases/scop_a3ms -hhmdir /databases/scop_hhms -oa3m /databases/scop_a3m_db -ohhm /databases/scop_hhm_db
+   perl create_db.pl -a3mdir /databases/scop_a3ms -hhmdir /databases/scop_hhms -oa3m /databases/scop -ohhm /databases/scop
 
-   perl create_db.pl -hhmdir /databases/scop_hhms -ohhm /databases/scop_hhm_db
+   perl create_db.pl -hhmdir /databases/scop_hhms -ohhm /databases/scop
 \n";
 
 # Variable declarations
@@ -100,13 +100,13 @@ for (my $i=0; $i<@ARGV; $i++) {
 	}
     } elsif ($ARGV[$i] eq "-oa3m") {
 	if (++$i<@ARGV) {
-	    $a3mfile=$ARGV[$i];
+	    $a3mfile=$ARGV[$i] . "_a3m_db";
 	} else {
 	    die ("$help\n\nERROR! Missing filename after -oa3m option!\n");
 	}
     } elsif ($ARGV[$i] eq "-ohhm") {
 	if (++$i<@ARGV) {
-	    $hhmfile=$ARGV[$i];
+	    $hhmfile=$ARGV[$i] . "_hhm_db";
 	} else {
 	    die ("$help\n\nERROR! Missing filename after -ohhm option!\n");
 	}
