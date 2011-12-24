fcc7ae47e3bce1b112b920bff7eb4dda834f0389
Andy Hauser <hauser@genzentrum.lmu.de>
2011-12-24 09:50:20 +0100

    Add cs and context variables here.

--- dist-scripts/HHPaths.pm
+++ dist-scripts/HHPaths.pm
@@ -10,12 +10,10 @@ our $VERSION = 2.00;
 our @ISA     = qw(Exporter);
 our @EXPORT  = qw($hhlib $hhdata $dummydb $perl $hhblits $dsspdir $dssp $pdbdir $ncbidir $execdir $datadir);
 
+
 # Please set HHLIB 
 our $hhlib   = $ENV{"HHLIB"};     # hh perl scripts (addss.pl, reformat.pl etc.)
 our $hhdata  = $hhlib."/data"
-
-our $perl_dir= $ENV{"HHLIB"}."/scripts";   # hh perl scripts (addss.pl, reformat.pl etc.)
-$ENV{"PATH"} = $perl_dir.":".$ENV{"PATH"}; # Add Perl scripts directory to PATH
 
 # PSIPRED etc
 our $execdir = ".../psipred/bin";        # Where the PSIPRED V2 programs have been installed
@@ -30,5 +28,16 @@ our $ncbidir = ".../blast/bin";          # Where the NCBI programs have been ins
 our $pdbdir  =  ".../pdb/all";            # where are the pdb files? Used in hhmakemodel.pl.
 our $dsspdir =  ".../dssp/data";          # where are the dssp files? Used in addss.pl.
 our $dssp    =  ".../dssp/bin/dsspcmbi";  # where is the dssp binary? Used in addss.pl.
+
+
+# Below probably does not need to be edited.
+
+# Essential databases
+our $cs_lib = "$hhdata/cs219.lib";
+our $context_lib = "$ahhdata/context_data.lib";
+
+# Perl
+our $perl_dir= $ENV{"HHLIB"}."/scripts";   # hh perl scripts (addss.pl, reformat.pl etc.)
+$ENV{"PATH"} = $perl_dir.":".$ENV{"PATH"}; # Add Perl scripts directory to PATH
 
 return 1;
