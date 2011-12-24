4843a23d38b95e9c7e6e9dda6d1ecde811294865
Andy Hauser <hauser@genzentrum.lmu.de>
2011-12-24 09:41:09 +0100

    Reflect rename MyPaths.pm to HHPaths.pm.

--- dist-scripts/HHPaths.pm
+++ dist-scripts/HHPaths.pm
@@ -1,5 +1,9 @@
-package MyPaths;
+#
+# Please edit and adjust to your local environment.
+#
+package HHPaths;
 
+# Skip this
 use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
 use Exporter;
 our $VERSION = 2.00;
@@ -7,17 +11,17 @@ our @ISA     = qw(Exporter);
 our @EXPORT  = qw($hhlib $hhdata $dummydb $perl $hhblits $dsspdir $dssp $pdbdir $ncbidir $execdir $datadir);
 
 # Please set HHLIB 
-our $hhlib   = $ENV{"HHLIB"}; # hh perl scripts (addss.pl, reformat.pl etc.)
+our $hhlib   = $ENV{"HHLIB"};     # hh perl scripts (addss.pl, reformat.pl etc.)
 our $hhdata  = $hhlib."/data"
 
-our $perl    = $ENV{"HHLIB"}."/scripts"; # hh perl scripts (addss.pl, reformat.pl etc.)
-$ENV{"PATH"} = $perl.":".$ENV{"PATH"}; # Add Perl scripts directory to PATH
+our $perl_dir= $ENV{"HHLIB"}."/scripts";   # hh perl scripts (addss.pl, reformat.pl etc.)
+$ENV{"PATH"} = $perl_dir.":".$ENV{"PATH"}; # Add Perl scripts directory to PATH
 
 # PSIPRED etc
 our $execdir = ".../psipred/bin";        # Where the PSIPRED V2 programs have been installed
 our $datadir = ".../psipred/data";       # Where the PSIPRED V2 data files have been installed    
 our $hmmerdir= ".../hmmer/binaries";
-our $dummydb = "$hhdata/dummydb"; # Name of a dummy blast database (single sequence formatted with formatdb)
+our $dummydb = "$hhdata/do_not_delete";  # Name of a dummy blast database (single sequence formatted with formatdb)
 
 # BLAST
 our $ncbidir = ".../blast/bin";          # Where the NCBI programs have been installed (for PSIPRED in addss.pl)
