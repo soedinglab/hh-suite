b13edc307c5a7fa0bb9e81768e9cd110ecb9dccc
soeding <soeding@genzentrum.lmu.de>
2012-01-26 12:45:33 +0100

    Changed position of #include SSE2 in hhalign to avoid compilation error due to moved _mm_flog2_ps()

--- src/hhalign.C
+++ src/hhalign.C
@@ -48,13 +48,22 @@
 //#include "efence.h"
 //#include "efence.c"
 
+#ifdef HH_SSE4
+#include <tmmintrin.h>   // SSSE3
+#include <smmintrin.h>   // SSE4.1
+#define HH_SSE3
+#endif
+
 #ifdef HH_SSE3
-#ifdef __SUNPRO_C
-#include <sunmedia_intrin.h>
-#else
-#include <emmintrin.h>   // SSE2
 #include <pmmintrin.h>   // SSE3
-///#include <smmintrin.h>   // SSE4.1
+#define HH_SSE2
+#endif
+
+#ifdef HH_SSE2
+#ifndef __SUNPRO_C
+#include <emmintrin.h>   // SSE2
+#else
+#include <sunmedia_intrin.h>
 #endif
 #endif
 
@@ -90,22 +99,6 @@ using std::ofstream;
 #include "hhfullalignment.C" // class FullAlignment
 #include "hhhitlist.C"   // class HitList
 #include "hhfunc.C"      // some functions common to hh programs
-
-#ifdef HH_SSE4
-#include <tmmintrin.h>   // SSSE3
-#include <smmintrin.h>   // SSE4.1
-#define HH_SSE3
-#endif
-
-#ifdef HH_SSE3
-#ifndef __SUNPRO_C
-#include <emmintrin.h>   // SSE2
-#include <pmmintrin.h>   // SSE3
-#else
-#include <sunmedia_intrin.h>
-#endif
-#endif
-
 
 #ifdef HH_PNG
 #include "pngwriter.h"   //PNGWriter (http://pngwriter.sourceforge.net/)
