3dd3d3d4a6a4e9459b56b5c946de8bf9ba32a8fb
meiermark <meiermark@genzentrum.lmu.de>
2014-04-04 09:28:08 +0200

    missed two headers

---
+++ src/hhfunc.h
@@ -0,0 +1,41 @@
+/*
+ * hhfunc.h
+ *
+ *  Created on: Apr 4, 2014
+ *      Author: meiermark
+ */
+
+#ifndef HHFUNC_H_
+#define HHFUNC_H_
+
+#include <cstdio>
+#include <iostream>
+#include "hhhmm.h"
+#include "hhhit.h"
+#include "hhalignment.h"
+
+void ReadQueryFile(FILE* inf, char& input_format, char use_global_weights, HMM* q, Alignment* qali, char infile[]);
+void ReadQueryFile(char* infile, char& input_format, char use_global_weights, HMM* q, Alignment* qali);
+
+// Add transition and amino acid pseudocounts to query HMM, calculate aa background etc.
+void PrepareQueryHMM(char& input_format, HMM* q);
+
+// Do precalculations for q and t to prepare comparison
+void PrepareTemplateHMM(HMM* q, HMM* t, int format);
+
+// Calculate secondary structure prediction with PSIPRED
+void CalculateSS(char *ss_pred, char *ss_conf, char *tmpfile);
+
+// Calculate secondary structure for given HMM and return prediction
+void CalculateSS(HMM* q, char *ss_pred, char *ss_conf);
+
+// Calculate secondary structure for given HMM
+void CalculateSS(HMM* q);
+
+// Write alignment in tab format (option -atab)
+void WriteToAlifile(FILE* alitabf, Hit* hit);
+
+// Read number of sequences in annotation, after second '|'
+int SequencesInCluster(char* name);
+
+#endif /* HHFUNC_H_ */
