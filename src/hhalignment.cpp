4e86f668e0f6b222e98f49858e691f4f7a2085ee
meiermark <meiermark13@gmail.com>
2016-12-19 23:59:55 +0100

    added offset for gap-ruled match state assignment

--- src/hhalignment.cpp
+++ src/hhalignment.cpp
@@ -1100,8 +1100,22 @@ void Alignment::Compress(const char infile[], const char cons, const int maxres,
         h[k] = 1;
         seq[k][0] = '-';
       }
+
+      int border = 0;
+      if(L <= 20) {
+        border = 0;
+      }
+      else if(L <= 40) {
+        border = 20;
+      }
+      else {
+        border = 40;
+      }
+
       for (l = 1; l <= L; l++) {
-        if (percent_gaps[l] <= float(Mgaps)) {
+        // for first and last columns look for slightly shifted columns for nr gaps
+        if ((l <= L - border && percent_gaps[std::max(border, l)] <= float(Mgaps)) ||
+            (l > border && percent_gaps[std::min(L - border + 1, l)] <= float(Mgaps))) {
           if (i >= maxres - 2) {
             HH_LOG(WARNING)
                 << "Number of match columns too large. Only first "
