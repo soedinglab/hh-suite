61865c0336e831a19e797139fb1d62ea6aee03b0
Milot Mirdita <milot@mirdita.de>
2019-04-04 11:38:20 +0000

    Fix C name mangling issue with fmemopen.h in C++ code

--- lib/ffindex/src/ext/fmemopen.h
+++ lib/ffindex/src/ext/fmemopen.h
@@ -1,5 +1,10 @@
 #ifndef __FMEMOPEN_H__
 #define __FMEMOPEN_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,4 +18,9 @@ typedef struct fmem fmem_t;
 
 FILE *fmemopen(void *, size_t, const char *);
 
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* __FMEMOPEN_H__ */
+
