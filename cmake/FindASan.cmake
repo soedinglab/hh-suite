dfe42d34ef39b42f17931b49d66eeafbee2cf5c2
Milot Mirdita <milot@mirdita.de>
2020-12-22 01:36:56 +0100

    Add ASanOpt and set ASan to not optimize

--- cmake/FindASan.cmake
+++ cmake/FindASan.cmake
@@ -34,7 +34,6 @@ include(CheckCXXCompilerFlag)
 # Set -Werror to catch "argument unused during compilation" warnings
 set(CMAKE_REQUIRED_FLAGS "-Werror -fsanitize=address") # Also needs to be a link flag for test to pass
 check_cxx_compiler_flag("-fsanitize=address" HAVE_FLAG_SANITIZE_ADDRESS)
-
 unset(CMAKE_REQUIRED_FLAGS)
 
 if(HAVE_FLAG_SANITIZE_ADDRESS)
@@ -50,10 +49,10 @@ endif()
 
 set(HAVE_ADDRESS_SANITIZER TRUE)
 
-set(CMAKE_C_FLAGS_ASAN "-O1 -g ${ADDRESS_SANITIZER_FLAG} -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+set(CMAKE_C_FLAGS_ASAN "-O0 -g ${ADDRESS_SANITIZER_FLAG} -fno-omit-frame-pointer -fno-optimize-sibling-calls"
     CACHE STRING "Flags used by the C compiler during ASan builds."
     FORCE)
-set(CMAKE_CXX_FLAGS_ASAN "-O1 -g ${ADDRESS_SANITIZER_FLAG} -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+set(CMAKE_CXX_FLAGS_ASAN "-O0 -g ${ADDRESS_SANITIZER_FLAG} -fno-omit-frame-pointer -fno-optimize-sibling-calls"
     CACHE STRING "Flags used by the C++ compiler during ASan builds."
     FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_ASAN "${ADDRESS_SANITIZER_FLAG}"
@@ -67,3 +66,25 @@ mark_as_advanced(CMAKE_C_FLAGS_ASAN
                  CMAKE_CXX_FLAGS_ASAN
                  CMAKE_EXE_LINKER_FLAGS_ASAN
                  CMAKE_SHARED_LINKER_FLAGS_ASAN)
+
+
+check_cxx_compiler_flag("-Og" HAVE_OPTIMIZE_DEBUG)
+if(HAVE_OPTIMIZE_DEBUG)
+  set(CMAKE_C_FLAGS_ASANOPT "-Og -g ${ADDRESS_SANITIZER_FLAG} -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+          CACHE STRING "Flags used by the C compiler during ASan Optimized builds."
+          FORCE)
+  set(CMAKE_CXX_FLAGS_ASANOPT "-Og -g ${ADDRESS_SANITIZER_FLAG} -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+          CACHE STRING "Flags used by the C++ compiler during ASan Optimized builds."
+          FORCE)
+  set(CMAKE_EXE_LINKER_FLAGS_ASANOPT "${ADDRESS_SANITIZER_FLAG}"
+          CACHE STRING "Flags used for linking binaries during ASan Optimized builds."
+          FORCE)
+  set(CMAKE_SHARED_LINKER_FLAGS_ASANOPT "${ADDRESS_SANITIZER_FLAG}"
+          CACHE STRING "Flags used by the shared libraries linker during ASan Optimized builds."
+          FORCE)
+
+  mark_as_advanced(CMAKE_C_FLAGS_ASANOPT
+          CMAKE_CXX_FLAGS_ASANOPT
+          CMAKE_EXE_LINKER_FLAGS_ASANOPT
+          CMAKE_SHARED_LINKER_FLAGS_ASANOPT)
+endif()
