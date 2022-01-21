5ae14ea7f384e5d41f53c678c34daf62b1770618
fuji8 <haya10.88ha@gmail.com>
2022-01-21 16:16:21 +0900

    fix: sort->stable_sort

--- src/hhalignment.cpp
+++ src/hhalignment.cpp
@@ -1677,7 +1677,7 @@ int Alignment::Filter2(char keep[], int coverage, int qid, float qsc,
     ksort = new int[N_in];  // never reuse alignment object for new alignment with more sequences
     for (k = 0; k < N_in; ++k)
       ksort[k] = k;
-    std::sort(ksort + kfirst + 1 , ksort+ N_in, [this](int a, int b){ return nres[a] > nres[b];});  //Sort sequences after kfirst (query) in descending order
+    std::stable_sort(ksort + kfirst + 1 , ksort+ N_in, [this](int a, int b){ return nres[a] > nres[b];});  //Sort sequences after kfirst (query) in descending order
   }
   for (kk = 0; kk < N_in; ++kk) {
     inkk[kk] = in[ksort[kk]];
